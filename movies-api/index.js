79890badb83160da43b6978df5fcf7f219b03ada
Blah <blah@gmail.com>
2022-05-02 23:34:25 +0100

    Week 10 Lab A exercise done

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genreRouter from './api/genres';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres',genreRouter);
 app.use(express.json());
 
 app.listen(port, () => {
